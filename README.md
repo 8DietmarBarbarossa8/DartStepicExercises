c1cce02fbe91ef3a33c65088674a39f64b1ff887
Dietmar <tcodmos@gmail.com>
2022-08-12 13:19:45 +0300

    init pro

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DartStepicExercises
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/dart_stepic_exercises_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_stepic_exercises/dart_stepic_exercises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
